002799418d7e3b04006eafa37ea0f5d4ffd95b54
Edgar Z. Alvarenga <edgar@ymonad.com>
2015-01-13 20:31:28 -0200

    Correcting static files url for receita blueprint

--- gastosabertos/receita/views.py
+++ gastosabertos/receita/views.py
@@ -3,18 +3,18 @@
 import os
 import pandas as pd
 
-from flask import (Blueprint, render_template, current_app, request,
-                   flash, url_for, redirect, session, abort)
+from flask import (Blueprint, render_template)
 
 receita = Blueprint('receita', __name__,
-                    template_folder="templates",
-                    static_folder='static')
+                    template_folder='templates',
+                    static_folder='static',
+                    static_url_path='/receita/static')
 
-csv_receita = os.path.join(receita.root_path, "static", "receita-2008-01.csv")
+csv_receita = os.path.join(receita.root_path, 'static', 'receita-2008-01.csv')
 df_receita = pd.read_csv(csv_receita, encoding='utf8')
 
-@receita.route("/receita")
-@receita.route("/receita/<int:page>")
+@receita.route('/receita')
+@receita.route('/receita/<int:page>')
 def receita_table(page=0):
     receita_data = df_receita.iterrows()
 
