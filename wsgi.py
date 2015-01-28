fd40bff1cdc2bb5c1178053653cccf9f18020345
Edgar Z. Alvarenga <edgar@ymonad.com>
2015-01-27 23:43:43 -0200

    Change wsgi filename and add static files of receita blueprint to package

---
+++ wsgi.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys, os, pwd
+
+project = "gastosabertos"
+
+# Use instance folder, instead of env variables.
+# specify dev/production config
+#os.environ['%s_APP_CONFIG' % project.upper()] = ''
+# http://code.google.com/p/modwsgi/wiki/ApplicationIssues#User_HOME_Environment_Variable
+#os.environ['HOME'] = pwd.getpwuid(os.getuid()).pw_dir
+
+BASE_DIR = os.path.join(os.path.dirname(__file__))
+# activate virtualenv
+activate_this = os.path.join(BASE_DIR, "../.virtualenvs/ga_py/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
+# give wsgi the "application"
+from gastosabertos import create_app
+application = create_app()
