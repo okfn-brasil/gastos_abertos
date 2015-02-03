bbf3e1bbb8ccd7f0408b24dd7575588f6567c807
Edgar Z. Alvarenga <edgar@ymonad.com>
2015-02-03 02:22:48 -0200

    Change Revenu model column types for 'code' column and 'description' for MySQL

--- gastosabertos/receita/models.py
+++ gastosabertos/receita/models.py
@@ -10,8 +10,8 @@ class Revenue(db.Model):
     __tablename__ = 'revenue'
 
     id = Column(db.Integer, primary_key=True)
-    code = Column(db.String(), nullable=False)
-    description = Column(db.String(), nullable=False)
+    code = Column(db.String(30), nullable=False)
+    description = Column(db.Text(), nullable=False)
     date = Column(db.Date())
     monthly_predicted = Column(db.DECIMAL(19,2))
     monthly_outcome = Column(db.DECIMAL(19,2))
