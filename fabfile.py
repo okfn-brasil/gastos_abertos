e09cc5e8d811fc64d1f4a54a3d40f4a93e8bbca1
Edgar Z. Alvarenga <edgar@ymonad.com>
2015-02-07 00:42:22 -0200

    Fab file modified to update automatically wsgi test server

--- fabfile.py
+++ fabfile.py
@@ -39,7 +39,8 @@ def deploy():
     project_dir = '/home/gastosabertos/gastos_abertos'
     with cd(project_dir):
         run("git pull")
-#        run("touch app.wsgi")
+        run("python setup.py install")
+        run("touch wsgi.py")
 
 def d():
     """
