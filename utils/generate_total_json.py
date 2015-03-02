152a277fc43fbbf3aff18fa46b55dafe6881acf6
Andres MRM <andresmrm@gmail.com>
2015-03-02 05:40:04 -0300

    orm_exc.NoResultFound? except all then...

--- utils/generate_total_json.py
+++ utils/generate_total_json.py
@@ -15,7 +15,7 @@ import json
 
 from docopt import docopt
 
-from sqlalchemy import func, extract, and_, orm
+from sqlalchemy import func, extract, and_
 # from sqlalchemy.orm.exc import NoResultFound
 
 from gastosabertos import create_app
@@ -45,7 +45,9 @@ def get_description(code, parent_description):
     try:
         description = db.session.query(RevenueCode.description)\
             .filter(RevenueCode.code == code_str).one()[0]
-    except orm.exc.NoResultFound:
+    # serves was raising 'orm_exc.NoResultFound'
+    # except NoResultFound:
+    except:
         if parent_description:
             description = parent_description
             # print(code, description)
