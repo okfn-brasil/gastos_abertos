9519608386b7a1f5274b593ecea99fc75ad61eaa
Andres MRM <andresmrm@gmail.com>
2015-07-14 21:29:41 -0300

    minimal working version of geocode_execucao

--- utils/geocode_execucao.py
+++ utils/geocode_execucao.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ''' Read a CSV with execucao and insert it in the DB.
+''' Geocodes the execucao table.
 
-# Usage:
-#     ./import_execucao [FOLDER] [LINES_PER_INSERT]
-#     ./import_execucao (-h | --help)
+Usage:
+    ./geocode_execucao [DATA_FOLDER] [TERMS_FOLDER] [LINES_PER_INSERT]
 
-# Options:
-#     -h --help   Show this message.
-# '''
+Options:
+    -h --help   Show this message.
+'''
 
 from __future__ import unicode_literals  # unicode by default
+
+import docopt
 
 from gastosabertos.execucao.models import Execucao
 from utils import ProgressCounter, get_db
@@ -32,6 +33,7 @@ def geocode_all(db, data_folder="geocoder/data",
                 terms_folder="geocoder/terms",
                 lines_per_insert=100):
 
+    print("Loading table...")
     # The query bellow seems not very efficient...
     # Maybe change it as the link says.
     # https://stackoverflow.com/questions/7389759/memory-efficient-built-in-sqlalchemy-iterator-generator
@@ -58,17 +60,16 @@ if __name__ == '__main__':
     db = get_db()
     geocode_all(db)
 
-    # arguments = docopt(__doc__)
-    # args = {}
-    # lines_per_insert = arguments['LINES_PER_INSERT']
-    # if lines_per_insert:
-    #     args['lines_per_insert'] = int(lines_per_insert)
+    arguments = docopt(__doc__)
+    args = {}
 
-    # folder = arguments['FOLDER']
-    # if not folder:
-    #     folder = "../../gastos_abertos_dados/Orcamento/execucao/"
+    lines_per_insert = arguments['LINES_PER_INSERT']
+    if lines_per_insert:
+        args['lines_per_insert'] = int(lines_per_insert)
 
-    # csvs = sorted([i for i in os.listdir(folder) if i[-4:] == ".csv"])
+    for arg in ["DATA_FOLDER", "TERMS_FOLDER"]:
+        folder = arguments[arg]
+        if folder:
+            args[arg.lower()] = folder
 
-    # for csv in csvs:
-    #     insert_csv(os.path.join(folder, csv), **args)
+    geocode_all(**args)
