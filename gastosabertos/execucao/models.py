6c0a3a6fe222d3b8ff871fcf4711c65405cdf89f
Andres MRM <andresmrm@gmail.com>
2015-08-17 19:02:02 -0300

    resolve #180

--- gastosabertos/execucao/models.py
+++ gastosabertos/execucao/models.py
@@ -13,11 +13,35 @@ class Execucao(db.Model):
 
     # id = Column(db.Integer, primary_key=True)
     code = Column(db.String(100), primary_key=True)
+    # Official data
     data = Column(postgresql.JSONB)
+    # Geodata about this expense line
     point = Column(Geometry('POINT'), default=None)
     # If an attempt were made to gecode this row
     searched = Column(db.Boolean, default=False)
-    # code = Column(db.String(50), nullable=False)
+
+    # Current state of this expense:
+    # _states_names = {
+    #     0: 'orcado',
+    #     1: 'atualizado',
+    #     2: 'empenhado',
+    #     3: 'liquidado',
+    # }
+    # _states_nums = {v: k for k, v in _states_names.items()}
+    # state = Column(db.Integer, default=0)
+    state = Column(db.Enum('orcado', 'atualizado', 'empenhado', 'liquidado',
+                           name='expense_state'),
+                   default=0)
+
+    # def get_state(self):
+    #     return Execucao._states_names[self.state]
+
+    # def set_state(self, state_name):
+    #     self.state = Execucao._states_nums[state_name]
+
+    # @classmethod
+    # def get_num_state(cls, state_name):
+    #     return Execucao._states_nums[state_name]
 
     @classmethod
     def get_year(cls):
